4d4c20cd15fa705ca56425639c41f041b3b8a0f2
Jesse Priest <68927440+jpcodigo@users.noreply.github.com>
2022-11-22 20:31:49 -0500

    Connected client to server and set up posts on FE

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,25 @@
 import './App.css';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [postList, setPostList] = useState([]);
+  useEffect(() => {
+    axios.get('http://localhost:8080/posts').then((response) => {
+      setPostList(response.data);
+    });
+  }, []);
   return (
     <div className="App">
-      
+      {postList.map((value, key) => {
+        return (
+          <div className="post">
+            <div className="title">{ value.title }</div>
+            <div className="body">{ value.postText }</div>
+            <div className="footer">{ value.username }</div>
+          </div>
+        )
+      })}
     </div>
   );
 }
