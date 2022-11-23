28ee1e236de3df5bec712fa7ff42b52140537464
Jesse Priest <68927440+jpcodigo@users.noreply.github.com>
2022-11-22 19:53:50 -0500

    Post and get routes for Posts table

--- server/server.js
+++ server/server.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
 const db = require('./models');
+
+// Routes
+const postRoutes = require('./routes/Posts');
+app.use('/posts', postRoutes);
 
 db.sequelize.sync().then(() => {
   app.listen(8080, () => {
